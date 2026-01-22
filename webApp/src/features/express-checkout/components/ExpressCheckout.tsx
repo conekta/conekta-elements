c3650f945eca7a5f5be94271ce6a96d86799a5ea
maoconekta <mauricio.arroyave@conekta.com>
2026-01-22 09:15:13 -0500

    fix build (#3)
    * fix: cleanup build
    
    * fix: cleanup build
    
    * config ci
    
    * fix node version
    
    * config node version
    
    * node version from file
    
    * config java version
    
    ---------
    
    Co-authored-by: Franklin Carrero <franklin.carrero@conekta.com>

--- webApp/src/features/express-checkout/components/ExpressCheckout.tsx
+++ webApp/src/features/express-checkout/components/ExpressCheckout.tsx
@@ -1,6 +1,9 @@
 import type { ExpressCheckoutProps } from '../types';
 
 export const ExpressCheckout = ({ publicKey, amount, currency }: ExpressCheckoutProps) => {
+  // TODO: Use publicKey for initializing payment gateway
+  console.log('Initializing with key:', publicKey);
+  
   return (
     <div>
       <h2>Express Checkout</h2>
