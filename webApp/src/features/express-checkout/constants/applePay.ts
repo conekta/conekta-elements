aba275de2c4bcc69d6c6505ad3d5c7ae62ca6d1c
Martín Agüera <maguera@conekta.com>
2026-01-26 11:37:44 -0300

    refactor: [AI-584] - moved Apple Pay button styles to a separate CSS file for better organization and maintainability

--- webApp/src/features/express-checkout/constants/applePay.ts
+++ webApp/src/features/express-checkout/constants/applePay.ts
@@ -8,33 +8,3 @@ export const APPLE_PAY_SUPPORTED_NETWORKS = ['amex', 'masterCard', 'visa'];
 export const APPLE_PAY_VERSION = 3;
 export const APPLE_PAY_COUNTRY_CODE_DEFAULT = 'MX';
 export const APPLE_PAY_MERCHANT_NAME_DEFAULT = 'Conekta';
-
-export const APPLE_PAY_BUTTON_STYLES = `
-  @supports (-webkit-appearance: -apple-pay-button) {
-    .apple-pay-button {
-      display: inline-block;
-      appearance: -apple-pay-button;
-      -webkit-appearance: -apple-pay-button;
-    }
-    .apple-pay-button-black {
-      -apple-pay-button-style: black;
-    }
-    .apple-pay-button-white {
-      -apple-pay-button-style: white;
-    }
-    .apple-pay-button-white-with-line {
-      -apple-pay-button-style: white-with-line;
-    }
-    .apple-pay-button-legacy {
-      display: none !important;
-    }
-  }
-  @supports not (-webkit-appearance: -apple-pay-button) {
-    .apple-pay-button {
-      display: none !important;
-    }
-    .apple-pay-button-legacy {
-      display: flex;
-    }
-  }
-`;
