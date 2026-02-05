0eaf6e4c8456526da5d064ead8e8b453a479e56f
Franklin <franklin.carrero@conekta.com>
2026-02-05 16:23:54 -0500

    Configures SonarCloud for improved code analysis
    Configures SonarCloud properties to ensure proper code analysis.
    
    - Updates the Kover report path to align with the single report output.
    - Adds an exclusion to the web app's coverage to prevent issues with code analysis.
    - Adds a debug step in CI to help determine where the coverage files are

--- webApp/vite.config.ts
+++ webApp/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       exclude: [
         'node_modules/',
         'dist/',
+        '.storybook/**',
         'storybook-static/',
         '**/*.stories.tsx',
         '**/*.config.ts',
