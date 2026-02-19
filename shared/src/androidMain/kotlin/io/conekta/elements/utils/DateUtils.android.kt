1eaeb77b090b7efe5ad0196c23173b288322398e
Franklin <franklin.carrero@conekta.com>
2026-02-19 15:10:20 -0500

    refactor: simplify currentTwoDigitYear function implementation in DateUtils.android.kt

--- shared/src/androidMain/kotlin/io/conekta/elements/utils/DateUtils.android.kt
+++ shared/src/androidMain/kotlin/io/conekta/elements/utils/DateUtils.android.kt
@@ -2,5 +2,4 @@ package io.conekta.elements.utils
 
 import java.util.Calendar
 
-actual fun currentTwoDigitYear(): Int =
-    Calendar.getInstance().get(Calendar.YEAR) % 100
+actual fun currentTwoDigitYear(): Int = Calendar.getInstance().get(Calendar.YEAR) % 100
