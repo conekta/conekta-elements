e610ea8d38ea7a69d243be0ca8c98dd6b314b458
Franklin <franklin.carrero@conekta.com>
2026-03-03 09:44:53 -0500

    chore: upgrade Storybook dependencies to version 8.6.0 and simplify main.ts configuration

--- webApp/.storybook/preview.tsx
+++ webApp/.storybook/preview.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
